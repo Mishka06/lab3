6c761e60c3c3493969ad0a5e261d97c10ce42532
Mishka06 <moihail2044mail.ru@gmail.com>
2022-04-06 21:39:04 +0300

    Initial commit

---
+++ src/lab.java
@@ -0,0 +1,13 @@
+public class lab {
+}
+class Main{
+
+    public static void main (String[]args) {
+        for (int a = 1; a <= 10; a++) {
+            for (int b = 1; b<= 10; b++) {
+                System.out.println(a+ " * " + b + " = " + a * b);
+            }
+            System.out.println();
+        }
+    }
+}
